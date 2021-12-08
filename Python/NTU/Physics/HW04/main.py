f204c1662c503ee62713c082e4c50e03c987926a
tom1484 <angelus0918866416@gmail.com>
2021-12-08 11:57:34 +0800

    Update

--- Python/NTU/Physics/HW04/main.py
+++ Python/NTU/Physics/HW04/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from vpython import *
 A, N, omega = 0.10, 50, 2*pi/1.0
-size, m, k, d = 0.06, 0.1, 10.0, 0.4
+size, m, k, d = 0.06, 0.1, 20.0, 0.4
 # scene = canvas(title='Spring Wave', width=800, height=300, background=vec(0.5,0.5,0), center = vec((N-1)*d/2, 0, 0))
 # balls = [sphere(radius=size, color=color.red, pos=vector(i*d, 0, 0), v=vector(0,0,0)) for i in range(N)] #3
 # springs = [helix(radius = size/2.0, thickness = d/15.0, pos=vector(i*d, 0, 0), axis=vector(d,0,0)) for i in range(N-1)] #3
