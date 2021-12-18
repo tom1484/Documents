d25f015fe590fc813d7b9c33f1c2bee0f2c5e86d
tom1484 <angelus0918866416@gmail.com>
2021-12-18 23:37:53 +0800

    'Update'

--- Python/test.py
+++ Python/test.py
@@ -1,5 +1,10 @@
-import os
-
-print(os.path.join('./', '123'))
-print(123)
-
+A = "F G H I J K L M N O P Q R S T U V W X Y Z AA AB".split(' ')
+R = ""
+for i in range(31):
+	c = input()
+	if c == "":
+		R += "\n"
+	else:
+		n = int(c)
+		R += A[n] + "\n"
+print(R)
