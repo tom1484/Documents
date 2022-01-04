885aab84dd69bd3ad3259020ee28fb05b9b02fe9
tom1484 <angelus0918866416@gmail.com>
2022-01-04 11:58:15 +0800

    Update

--- Python/test.py
+++ Python/test.py
@@ -1,10 +1,31 @@
-A = "F G H I J K L M N O P Q R S T U V W X Y Z AA AB".split(' ')
-R = ""
-for i in range(31):
-	c = input()
-	if c == "":
-		R += "\n"
-	else:
-		n = int(c)
-		R += A[n] + "\n"
-print(R)
+a = 0
+
+def f():
+	global a
+	a += 1
+	return True
+
+def check(i):
+	return i % 3 == 0
+
+# a = 1
+
+# def f():
+# 	global a
+# 	a *= 2
+# 	return True
+
+# def check(i):
+# 	return True
+
+N = 10000
+M = 950
+
+def r(i, I):
+	return (i <= I and i <= N) and ((check(i) and f() or True) and r(i + 1, I))
+
+def R(i, I):
+	return i <= I and ((r(i * M, (i + 1) * M - 1) or True) and R(i + 1, I))
+
+R(0, N // M)
+print(a)
