5de108f8dd71b607d244bacce2b49443aceecd70
tom1484 <angelus0918866416@gmail.com>
2022-06-20 21:44:47 +0800

    Update

--- Julia/test.jl
+++ Julia/test.jl
@@ -1,11 +1,23 @@
-function mandel(z)
-    c = z
-    max = 80
-    for n = 1:max
-        if abs(z) > 2
-            return n-1
-        end
-        z = z^2 + c
-    end
-    return max
-end
+# function mandel(z)
+#     c = z
+#     max = 80
+#     for n = 1:max
+#         if abs(z) > 2
+#             return n-1
+#         end
+#         z = z^2 + c
+#     end
+#     return max
+# end
+
+using PyCall
+
+py"""
+import matplotlib.pyplot
+
+print(123)
+plt.plot(collect(1:4))
+"""
+
+# pyimport("matplotlib.pyplot")
+
