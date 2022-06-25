1b3d0841a8c0cfce1fa2334c60c04c0954a0aee3
tom1484 <angelus0918866416@gmail.com>
2022-06-25 20:11:48 +0800

    Update

--- C/test.c
+++ C/test.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-
-	printf("123\n");
-
-	return 0;
+void m_malloc(int **p) {
+    *p = malloc(4);
 }
 
+int main(void) {
+
+    /* int arr[2][4] = { */
+    /*     {1, 2, 3, 4}, */ 
+    /*     {2, 3, 4, 5} */
+    /* }; */
+
+    /* char *restrict str1 = "1234"; */
+    /* char *restrict str2 = str1; */
+    int *p = 0;
+    printf("%p\n", p);
+    
+    m_malloc(&p);
+    printf("%p\n", p);
+
+    return 0;
+}
